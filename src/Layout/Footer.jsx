ed36456e72a7f07944bcbf20fa5995e84f707c6b
Ali <mohammad.98482@gmail.com>
2023-10-08 23:15:59 +0600

    Footer Section Added

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,8 +1,34 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo-dark.png'
+
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="bg-primary">
+            <div className="w-full container mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <Link to='/'><img className='w-24 mx-auto py-3 sm:py-0' src={Logo} alt="Unique Weddings" /></Link>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+                        <li>
+                            <Link className='mr-4 hover:underline md:mr-6'>About</Link>
+                        </li>
+                        <li>
+                            <Link className='mr-4 hover:underline md:mr-6'>Privacy Policy</Link>
+                        </li>
+                        <li>
+                            <Link className='mr-4 hover:underline md:mr-6'>Licensing</Link>
+                        </li>
+                        <li>
+                            <Link className='mr-4 hover:underline md:mr-6'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-pink-700 sm:mx-auto lg:my-8" />
+                <span className="block text-sm text-white sm:text-center">© {currentYear} Unique Weddings. All Rights Reserved.</span>
+            </div>
+        </footer>
+
+
     );
 };
 
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <header>
             <nav className="bg-primary w-full z-20">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="container flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to='/'><img className='w-24' src={Logo} alt="Unique Weddings" /></Link>
                     <div className="flex md:order-2">
                         <div className='flex gap-2'>
@@ -18,11 +18,11 @@ const Header = () => {
                         <button onClick={() => setOpened(!opened)} className='text-3xl text-white md:hidden'>{opened ? <FiMenu></FiMenu> : <FiX></FiX>}</button>
                     </div>
                     <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${opened == true ? 'hidden' : ''}`}>
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border md:flex-row md:space-x-8 md:mt-0 md:border-0">
-                            <li className='block py-2 pl-3 pr-4 cursor-pointer text-white md:p-0'><NavLink to='/' className={({isActive})=>isActive?'text-bold bg-white text-primary px-2 py-1 rounded':''}>Home</NavLink></li>
-                            <li className='block py-2 pl-3 pr-4 cursor-pointer text-white md:p-0'>Services</li>
-                            <li className='block py-2 pl-3 pr-4 cursor-pointer text-white md:p-0'>About</li>
-                            <li className='block py-2 pl-3 pr-4 cursor-pointer text-white md:p-0'>Contact</li>
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded md:flex-row md:space-x-8 md:mt-0 md:border-0">
+                            <li className='block py-2 pl-3 pr-4 cursor-pointer hover:underline text-white md:p-0'><NavLink to='/' className={({isActive})=>isActive?'text-bold bg-white text-primary px-2 py-1 rounded':''}>Home</NavLink></li>
+                            <li className='block py-2 pl-3 pr-4 cursor-pointer hover:underline text-white md:p-0'><NavLink to='/service' className={({isActive})=>isActive?'text-bold bg-white text-primary px-2 py-1 rounded':''}>Services</NavLink></li>
+                            <li className='block py-2 pl-3 pr-4 cursor-pointer hover:underline text-white md:p-0'><NavLink to='/about' className={({isActive})=>isActive?'text-bold bg-white text-primary px-2 py-1 rounded':''}>About</NavLink></li>
+                            <li className='block py-2 pl-3 pr-4 cursor-pointer hover:underline text-white md:p-0'><NavLink to='/contact' className={({isActive})=>isActive?'text-bold bg-white text-primary px-2 py-1 rounded':''}>Contact</NavLink></li>
                         </ul>
                     </div>
                 </div>
